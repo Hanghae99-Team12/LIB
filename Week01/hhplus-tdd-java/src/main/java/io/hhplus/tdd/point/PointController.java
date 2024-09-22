fd48b79ef06fb1173a0bc430dcded54c8f442849
nashs789 <nashs789@naver.com>
2024-09-23 00:24:27 +0900

    Feat: 각각의 엔드 포인트 기본 기능 구현

--- Week01/hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointController.java
+++ Week01/hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,16 +1,24 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static io.hhplus.tdd.point.TransactionType.*;
+
 @RestController
 @RequestMapping("/point")
+@RequiredArgsConstructor
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final PointHistoryTable pointHistoryTable;
+    private final UserPointTable userPointTable;
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
@@ -19,7 +27,7 @@ public class PointController {
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+        return userPointTable.selectById(id);
     }
 
     /**
@@ -29,7 +37,7 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        return pointHistoryTable.selectAllByUserId(id);
     }
 
     /**
@@ -38,9 +46,12 @@ public class PointController {
     @PatchMapping("{id}/charge")
     public UserPoint charge(
             @PathVariable long id,
-            @RequestBody long amount
+            @RequestBody ChargeRequest chargeRequest
     ) {
-        return new UserPoint(0, 0, 0);
+        long afterAmount = userPointTable.selectById(id).point() + chargeRequest.amount();
+        pointHistoryTable.insert(id, afterAmount, CHARGE, System.currentTimeMillis());
+
+        return userPointTable.insertOrUpdate(id, afterAmount);
     }
 
     /**
@@ -49,8 +60,11 @@ public class PointController {
     @PatchMapping("{id}/use")
     public UserPoint use(
             @PathVariable long id,
-            @RequestBody long amount
+            @RequestBody ChargeRequest chargeRequest
     ) {
-        return new UserPoint(0, 0, 0);
+        long afterAmount = userPointTable.selectById(id).point() - chargeRequest.amount();
+        pointHistoryTable.insert(id, afterAmount, USE, System.currentTimeMillis());
+
+        return userPointTable.insertOrUpdate(id, afterAmount);
     }
 }
